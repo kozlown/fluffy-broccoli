e90c1ac942148a4167c977ff3a5a56cc320acf84
emile <emile31.roch@gmail.com>
2018-02-23 10:28:34 +0100

    Dislpay profile information - refs #master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css'
-import Login from './Login'
-import Signup from './Signup'
-import Profile from './Profile'
+import "./App.css";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+import User from "./User";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
-          <div className='content'>
-            <Route exact path="/" component={Login}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/signup" component={Signup}/>
-            <Route path="/profile" component={Profile}/>
+        <div className="App">
+          <div className="content">
+            <Route exact path="/" component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/user" component={User} />
           </div>
         </div>
       </Router>
--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,49 @@
-import React from 'react'
-
-import './Profile.css'
+import React from "react";
+import Request from "react-http-request";
+import "./Profile.css";
 
 class Profile extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='Profile'>
-        <h1>
-          Profile
-        </h1>
-      </div>
-    )
+      <Request
+        url="http://localhost:8080/conducteur"
+        method="get"
+        accept="application/json"
+        verbose={true}
+      >
+        {({ error, result, loading }) => {
+          if (loading) {
+            return <div>loading...</div>;
+          } else {
+            return (
+              <div className="Profile">
+                <h1>Profile</h1>
+                {result.body.name ? (
+                  <div className="Profile__name">nom : {result.body.name}</div>
+                ) : (
+                  <div className="Profile__name">
+                    nous n'avons pas trouvé votre utilisateur
+                  </div>
+                )}
+                {result.body.mail ? (
+                  <div className="Profile__mail">
+                    vous pouvez contacter cet utilisateur par mail :
+                    {result.body.email}
+                  </div>
+                ) : null}
+                {result.body.tel ? (
+                  <div className="Profile__tel">
+                    ainsi que par tel:
+                    {result.body.tel}
+                  </div>
+                ) : null}
+              </div>
+            );
+          }
+        }}
+      </Request>
+    );
   }
 }
 
-export default Profile
+export default Profile;
